4a0c7584a5e88b7a21115154253cfffda6641f0d
vamshikiranm <vamshimorlawar@gmail.com>
2020-09-02 19:31:44 +0530

    Tests Passed

--- src/CashMachine.java
+++ src/CashMachine.java
@@ -20,6 +20,7 @@ public class CashMachine {
 
             amountDispensedFromTheMachine = amountEnteredByUser;
             this.amountOfCashInCashMachine -= amountDispensedFromTheMachine;
+
         }
     }
 
